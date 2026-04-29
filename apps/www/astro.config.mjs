c25c76d38340a6c991d0ef29aa48f1de111f1020
deepak <96181928+logsbydeepak@users.noreply.github.com>
2026-04-29 21:35:17 +0530

    fix www

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: "server",
+  adapter: vercel(),
   vite: {
     plugins: [tailwindcss()],
   },

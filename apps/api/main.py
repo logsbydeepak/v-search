e826940f554c5a1ef9e927e55b8f223baafaaa0c
deepak <96181928+logsbydeepak@users.noreply.github.com>
2026-04-29 12:46:59 +0530

    add db in api

--- apps/api/main.py
+++ apps/api/main.py
@@ -1,7 +1,19 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, g
+import os
 import tkz
+import psycopg
 
 app = Flask(__name__)
+
+DB_URL = os.getenv("DB_URL")
+if not DB_URL:
+    raise Exception("Missing DB_URL")
+
+
+def get_db():
+    if "db" not in g:
+        g.db = psycopg.connect(DB_URL)
+    return g.db
 
 
 @app.get("/search")
@@ -12,5 +24,32 @@ def hello_world():
         return jsonify(result)
 
     words = tkz.tokenize(query)
+
+    db = get_db()
+
+    rows = db.execute(
+        """
+        SELECT
+            p.url,
+            p.title,
+            p.description,
+            SUM(w.frequency) AS keyword_score,
+            COUNT(pl.from_url) AS backlinks,
+            SUM(w.frequency) + COUNT(pl.from_url) AS total_score
+        FROM word_index w
+        JOIN page p ON p.id = w.page_id
+        LEFT JOIN page_link pl ON pl.to_url = p.url
+        WHERE w.word = ANY(%s)
+        GROUP BY p.id, p.url, p.title
+        ORDER BY total_score DESC
+        LIMIT 10;
+        """,
+        (words,),
+    ).fetchall()
+
+    for row in rows:
+        result.append(
+            {"url": row["url"], "title": row["title"], "description": row["title"]}
+        )
 
     return result
